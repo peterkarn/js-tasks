d148d296f16cb77cb9b8ecfc9ce1208419729ad6
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-02 14:15:06 +0200

    task 5 complete

--- 1/task05.js
+++ 1/task05.js
@@ -1,13 +1,17 @@
 'use strict';
 
 // Написать функцию, которая принимает строку, а возвращает строку в обратном порядке
-function funcName(str) {
-    
-}
 
-Например: 
-funcName('world') == 'dlrow'
+
+// Например: 
+// funcName('world') == 'dlrow'
 
 
 
 // Solutions: 
+
+function funcName(str) {
+  return str.split('').reverse().join('');
+}
+
+funcName('world');
