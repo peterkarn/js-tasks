f21fef3dddaa2f2cb8b14f51eda0e93980fb36c0
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-03 00:30:29 +0200

    task 1 - 3 complete

--- 3/task01.js
+++ 3/task01.js
@@ -1,15 +1,17 @@
 'use strict';
 
 // Написать функцию, которая принимает строку, а возвращает изменненную строку без гласных
+
+// Например:   
+const str = "This website is for losers LOL!";
+// funcName(str), "Ths wbst s fr lsrs LL!"
+
 function funcName(str) {
-    
+  const reg = /[quioaey]/gi;
+  console.log(str.replace(reg, ""))
 }
 
-Например:   
-var str = "This website is for losers LOL!";
-funcName(str), "Ths wbst s fr lsrs LL!"
-
-
+funcName(str);
 
 
 // Solutions: 
