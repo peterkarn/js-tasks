8ba82d8634626cfd8797e811efab32cf5da3f6d0
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-02 23:19:12 +0200

    task 4 - 2 complete

--- 2/task04.js
+++ 2/task04.js
@@ -1,15 +1,28 @@
 'use strict';
 
 // Написать функцию, которые принимает массив, а возвращает сумму его значений
-function funcName(arr) {
-    
-}
 
 
-Например: 
-funcName([1, 3]),                 4
-funcName([1, 5.2, 4, 0, -1]),     9.2                              
-
-
+// Например: 
+// funcName([1, 3]),                 4
+// funcName([1, 5.2, 4, 0, -1]),     9.2                              
 
 // Solutions: 
+// 1
+// function funcName(arr) {
+//   const sum = arr.reduce((total, current) => {
+//     return total + current
+//   }, 0);
+//   console.log(sum);
+// }
+
+// funcName([1, 5.2, 4, 0, -1]);
+//2
+
+// function funcName(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum +=arr[i]
+//   }
+//   console.log(sum);
+// }
