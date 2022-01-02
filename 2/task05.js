f120f85fa294320e41342335f52c84bede41b7a7
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-02 23:59:48 +0200

    task 5 - 2 complete

--- 2/task05.js
+++ 2/task05.js
@@ -2,14 +2,18 @@
 
 // Написать функцию, которые принимает строку и удаляет из нее все восклицательные знаки
 // через regExp
-function funcName(str) {
-    
-}
 
 
-Например: 
-funcName("Hello World!"),        "Hello World"                            
-funcName("RrlVnvrynI!IkQplnQIFA rJHSRrPWGq!fzvfRNObQy"),        "RrlVnvrynIIkQplnQIFA rJHSRrPWGqfzvfRNObQy"  
+
+// Например: 
+// funcName("Hello World!"),        "Hello World"                            
+// funcName("RrlVnvrynI!IkQplnQIFA rJHSRrPWGq!fzvfRNObQy"),        "RrlVnvrynIIkQplnQIFA rJHSRrPWGqfzvfRNObQy"  
 
 
 // Solutions: 
+function funcName(str) {
+  const regEx = /!/g;
+  let newStr = str.replaceAll(regEx, "");
+  console.log(newStr);
+}
+funcName("Hello World!")
