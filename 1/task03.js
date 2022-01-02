5d3becd6c7ac2a275d0dcdb702ccaf4ac7e87daa
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-02 13:59:56 +0200

    task 3 complete

--- 1/task03.js
+++ 1/task03.js
@@ -1,15 +1,19 @@
 'use strict';
 
 // Написать функцию, которая принимает число, а возвращает строку(четное, нечетное) либо 'Even' либо 'Odd'
-function funcName(number) {
-    
-}
 
-Например: 
-funcName(5)  ->   'Odd'
-funcName(3)  ->   'Odd'
-funcName(2)  ->   'Even'
+
+// Например: 
+// funcName(5)  ->   'Odd'
+// funcName(3)  ->   'Odd'
+// funcName(2)  ->   'Even'
 
 
 
 // Solutions: 
+function funcName(number) {
+  if (number % 2 === 0) return 'even';
+  return 'odd';
+}
+
+funcName(10)
