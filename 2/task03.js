0ea0b09e014b3e762c1c1e500542c0a3e618b527
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-02 15:13:45 +0200

    task 3-1 completed

--- 2/task03.js
+++ 2/task03.js
@@ -1,16 +1,19 @@
 'use strict';
 
 // Написать функцию, которая принимает строку, а возвращает число 
-function funcName(str) {
-    
-}
 
-Например: 
-funcName("1234"),   1234
-funcName("605"),    605
-funcName("1405"),   1405
+// Например: 
+// funcName("1234"),   1234
+// funcName("605"),    605
+// funcName("1405"),   1405
 funcName("-7"),     -7
 
 
 
 // Solutions: 
+function funcName(str) {
+  console.log(
+    Number(str),
+    +str,
+  );
+}
