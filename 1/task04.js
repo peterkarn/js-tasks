8e986588b64458cf9bb0e69ccafc1058525189d6
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-02 14:10:36 +0200

    task 4 complete

--- 1/task04.js
+++ 1/task04.js
@@ -1,14 +1,20 @@
 'use strict';
 
 // Написать функцию, которая принимает число повторений(n) и строку, а возвращает строку N раз
-function funcName(n, str) {
-    
-}
 
-Например: 
-funcName(6, "I") // "IIIIII"
-funcName(5, "Hello") // "HelloHelloHelloHelloHello"
+// Например: 
+// funcName(6, "I") // "IIIIII"
+// funcName(5, "Hello") // "HelloHelloHelloHelloHello"
 
 
 
 // Solutions: 
+function funcName(n, str) {
+  let resultString = '';
+  for (let i = 0; i < n; i++) {
+    resultString += str;
+  }
+  console.log(resultString);
+}
+
+funcName(5, 'Hi');
