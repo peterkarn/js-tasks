014607b0887a2a265a8aa05f7217b40c376433c8
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-04 19:13:09 +0200

    task 3 - 3 complete

--- 3/task02.js
+++ 3/task02.js
@@ -1,18 +1,24 @@
 'use strict';
 
 // Написать функцию, которая принимает строку, а возвращает измененную строку с дублирующими символами
-function funcName(str) {
-    
-}
 
-Например:   
-funcName("abcd"),       "aabbccdd"
-funcName("Adidas"),     "AAddiiddaass"
-funcName("1337"),       "11333377");
-funcName("illuminati"), "iilllluummiinnaattii"
-funcName("123456"),     "112233445566"
-funcName("%^&*("),      "%%^^&&**(("
+// Например:   
+// funcName("abcd"),       "aabbccdd"
+// funcName("Adidas"),     "AAddiiddaass"
+// funcName("1337"),       "11333377";
+// funcName("illuminati"), "iilllluummiinnaattii"
+// funcName("123456"),     "112233445566"
+// funcName("%^&*("),      "%%^^&&**(("
 
 
 
 // Solutions: 
+
+function funcName(str) {
+    const newStr = str.split('').map(element => {
+      return element + element;
+    }).join('');
+  console.log(newStr);
+}
+
+funcName("123456")
