b8d5f1a5b8b573143f4c4c76f87d0390db81d709
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-04 20:26:02 +0200

    task 5 - 3 complete

--- 3/task05.js
+++ 3/task05.js
@@ -1,13 +1,17 @@
 'use strict';
 
 // Написать функцию, которая принимает число, а возвращает массив с разбитыми на части числами
+
+
+// Например:   
+// funcName(35231),     [1,3,2,5,3]
+
 function funcName(number) {
-    
+  console.log(
+    ('' + number).split('').map(i => {
+      return +i
+    })
+  );
 }
 
-Например:   
-funcName(35231),     [1,3,2,5,3]
-
-
-
-// Solutions: 
+funcName(35231);
