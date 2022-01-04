7b97db87e942e2962c830fe8cd7e1026c9e44065
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-04 19:18:27 +0200

    task 3 - 3 complete

--- 3/task03.js
+++ 3/task03.js
@@ -1,14 +1,18 @@
 'use strict';
 
 // Написать функцию, которая принимает 1 параметр, а возвращает функцию при вызове которой вернется заданный параметр
+
+// Например:   
+// funcName("abcd")()       "abcd"
+// funcName(true)()         true
+
 function funcName(arg) {
-    
+  function newFunc(argument) {
+    return argument
+  }
+  return newFunc(arg);
 }
-
-Например:   
-funcName("abcd")()       "abcd"
-funcName(true)()         true
-
+funcName(true)
 
 
 
