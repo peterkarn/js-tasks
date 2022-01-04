98f8aa89ed39afde1f6b7c005d9e73845afd98c0
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-04 20:15:23 +0200

    task 4 - 3 complete

--- 3/task04.js
+++ 3/task04.js
@@ -1,14 +1,24 @@
 'use strict';
 
 // Написать функцию, которая принимает строку, а возвращает изменненную строку с заглавными буквами
+
+
+// Например:   
+const str = "How can mirrors be real if our eyes aren't real";
+// funcName(str), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+
 function funcName(str) {
-    
+  console.log(
+
+    str.split(' ').map(str => {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+      //вернули 1 символ каждой строки, капитализировали, приклеили остаток строки без первого символа
+    }).join(' ')
+
+  );
 }
 
-Например:   
-var str = "How can mirrors be real if our eyes aren't real";
-funcName(str), "How Can Mirrors Be Real If Our Eyes Aren't Real");
-
+funcName(str);
 
 
 
