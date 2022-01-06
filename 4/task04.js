2470200696aa8f98bdcd82fffc2f94fccca53f5c
peterkarnaukhsite <62539689+peterkarnaukhsite@users.noreply.github.com>
2022-01-06 11:16:29 +0200

    Update task04.js

--- 4/task04.js
+++ 4/task04.js
@@ -3,16 +3,29 @@
 // Написать функцию, которая принимает строку, а возвращает средину строки при таких условиях:
 // если длина строки нечетная то возвращать 1 символ
 // если четная то 2 символа
+
+
+// Например:   
+// funcName("test"),    "es"
+// funcName("testing"), "t"
+// funcName("middle"),  "dd"
+// funcName("A"),       "A"
+
+
+
+// Solutions:
+
+
 function funcName(str) {
-    
+  const length = str.length;
+  const middlePosition = length / 2;
+
+  function extract(symbols) {
+    return str.substr(middlePosition, symbols);
+  }
+  
+  return length % 2 === 0 ? extract(2) : extract(1)
+
 }
 
-Например:   
-funcName("test"),    "es"
-funcName("testing"), "t"
-funcName("middle"),  "dd"
-funcName("A"),       "A"
-
-
-
-// Solutions: 
+funcName("A")
