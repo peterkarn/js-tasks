6b4c97f08ab50ef4fcc63b28e74408a5bf408acf
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-07 17:47:25 +0200

    task 1 - 5 complete

--- 5/task01.js
+++ 5/task01.js
@@ -1,16 +1,20 @@
 'use strict';
 
 // Написать функцию, которая принимает в параметры Операцию(строка) и 2 числа, а возвращает результат указанной операции с 2 чисел
-function funcName(operation, value1, value2) {
-    
-}
 
-Например:   
-funcName('+', 4, 7),       11 
-funcName('-', 15, 18),     -3 
-funcName('*', 5, 5),       25 
-funcName('/', 49, 7),      7 
+// Например:   
+// funcName('+', 4, 7),       11 
+// funcName('-', 15, 18),     -3 
+// funcName('*', 5, 5),       25 
+// funcName('/', 49, 7),      7 
 
 
 
 // Solutions: 
+
+function funcName(operation, value1, value2) {
+  const regex = /[\+\-\*\/]/
+  console.log(operation.match(regex) ? eval('' + value1 + operation + +value2) : 'Enter correct math operator like + - * /')
+}
+
+funcName('a', 49, 7);
