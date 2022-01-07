5423f04e99f4a6a076f51abc29bc50b9e4cfaa80
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-07 17:14:53 +0200

    task 5 - 4 complete

--- 4/task05.js
+++ 4/task05.js
@@ -1,14 +1,22 @@
 'use strict';
 
 // Написать функцию, которая принимает число, а возвращает сумму чисел от 1 до заданного в функции числа
-function funcName(number) {
-    
-}
 
-Например:   
-funcName(2),     1 + 2  ->  3
-funcName(8),     1 + 2 + 3 + 4 + 5 + 6 + 7 + 8    ->     36 
+
+// Например:   
+// funcName(2),     1 + 2  ->  3
+// funcName(8),     1 + 2 + 3 + 4 + 5 + 6 + 7 + 8    ->     36 
 
 
 
 // Solutions: 
+
+function funcName(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum += i
+  }
+  return sum
+}
+
+funcName(8)
