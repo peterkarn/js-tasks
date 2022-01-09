c27eb5acc9ca1b52c5e6bf3d286a86dfb848ed13
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-09 12:32:41 +0200

    task 2 - 5 complete

--- 5/task02.js
+++ 5/task02.js
@@ -1,14 +1,32 @@
 'use strict';
 
 // Написать функцию, которая принимает массив, а возвращает новый массив без дупликатов
-function funcName(arr) {
-    
-}
 
-Например:   
-let arr = [1, 3, 5, 3, 6, 7, 2]      ->    [1, 2, 3, 5, 6, 7]
+// Например:
+// let arr = [1, 3, 5, 3, 6, 7, 2]      ->    [1, 2, 3, 5, 6, 7]
 
 
 
-// Solutions: 
+// Solutions:
 
+let arr = [1, 3, 5, 3, 6, 7, 2, 44];
+
+// function funcName(a) {
+//   const newArr = new Set(a);
+//   return Array.from(newArr)
+//   return ...newArr
+// }
+
+// function funcName(a) {
+//   let arr2 = a.filter((item, i) => arr.indexOf(item) === i);
+//   return arr2.sort((a, b) => a - b)
+// }
+
+// function funcName(a) {
+//   let arr2 = a.reduce((sum, item) => {
+//     return sum.includes(item) ? sum : [... result, item]
+//   }, [])
+//   return arr2
+// }
+
+funcName(arr);
