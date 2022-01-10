aa37c3e9a461e6305f3acd8f82336e394280d531
peterkarnaukhsite <peterkarnaukh@gmail.com>
2022-01-10 22:00:21 +0200

    task 4-5 & 5-5 completed

--- 5/task04.js
+++ 5/task04.js
@@ -1,15 +1,18 @@
 'use strict';
 
 // Написать функцию, которая принимает строку, а возвращает длину самого короткого слова
-function funcName(str) {
-    
+
+
+// Например:  
+// funcName("bitcoin take over the world maybe who knows perhaps"),                3
+// funcName("turns out random test cases are easier than writing out basic ones"), 3
+
+const funcName = (str) => {
+  const sortedArrByLength = str.split(' ').sort((a, b) => {
+    return a.length - b.length
+  });
+  return sortedArrByLength[0].length
 }
 
-Например:  
-funcName("bitcoin take over the world maybe who knows perhaps"),                3
-funcName("turns out random test cases are easier than writing out basic ones"), 3
-
-
-
-// Solutions: 
+funcName("bitcoin take over the world maybe who knows perhaps")
 
